0c1b3221ba63072b6dafc56b73d9411afb537219
LevaiLevente321 <levike-2000@freemail.hu>
2021-04-20 16:10:38 +0200

    Extrás.

--- app/src/main/java/com/example/android_robi_es_david/SecondActivity.java
+++ app/src/main/java/com/example/android_robi_es_david/SecondActivity.java
@@ -7,13 +7,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ImageView;
 
+import java.text.BreakIterator;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView product = findViewById(R.id.product);
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            product.setText("Köszönjük a következő termék vásárlást!" + extras.get("usd").toString());
         }
 
     }
+}
 
