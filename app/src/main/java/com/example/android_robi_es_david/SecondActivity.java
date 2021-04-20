b3f1d8670c7eca22ce3c55512eb052f31eb0219f
LevaiLevente321 <levike-2000@freemail.hu>
2021-04-20 15:49:56 +0200

    SecondActivity.java és activity_second.xml hozzáadva.

---
+++ app/src/main/java/com/example/android_robi_es_david/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.android_robi_es_david;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        }
+
+    }
+
